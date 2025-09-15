e281a0fe0709d2c031e418105b0f41cb9a4bb50f
Hargaflarb <emmabrocher@gmail.com>
2025-09-15 12:22:17 +0200

    finished up additive scenes, scenes can now be activated & deactivated in the editor

--- Assets/Core (nix pille!)/Scripts/SceneManaging.cs
+++ Assets/Core (nix pille!)/Scripts/SceneManaging.cs
@@ -6,22 +6,45 @@ public class sceneManaging : MonoBehaviour
 {
     #region Fields
     //make sure these are the same as the scene names, at least in the editor
+    [Header("Main scene")]
     [SerializeField]
+    [Tooltip("No switch for this one, Main Scene is what holds everything else, so should always be activated.")]
     string mainSceneName = "Main Game";
+    [Header ("Menu")]
     [SerializeField]
     string mainMenuSceneName = "Main menu";
     [SerializeField]
+    bool activateMainMenu = false;
+    [Header("UI")]
+    [SerializeField]
     string uiSceneName = "UI";
     [SerializeField]
+    bool activateUI = true;
+    [Header("Sorting minigame")]
+    [SerializeField]
     string sortingGameSceneName = "Sorting game";
+    [SerializeField]
+    bool activateSortingGame = false;
     #endregion
     #region Methods
     private IEnumerator Start()
     {
         //load all the scenes you want
-        SceneManager.LoadScene(mainSceneName,LoadSceneMode.Additive);
-        SceneManager.LoadScene(uiSceneName,LoadSceneMode.Additive);
-        //unload this scene, as it is not needed
+        SceneManager.LoadScene(mainSceneName, LoadSceneMode.Additive);
+        if (activateMainMenu) 
+        {
+            SceneManager.LoadScene(mainMenuSceneName, LoadSceneMode.Additive);
+        }
+        if (activateUI)
+        {
+            SceneManager.LoadScene(uiSceneName, LoadSceneMode.Additive);
+        }
+        if(activateSortingGame)
+        {
+            SceneManager.LoadScene(sortingGameSceneName, LoadSceneMode.Additive);
+        }
+        
+        //unload this scene, as it is not needed anymore
         yield return SceneManager.UnloadSceneAsync(gameObject.scene);
     }
     #endregion
