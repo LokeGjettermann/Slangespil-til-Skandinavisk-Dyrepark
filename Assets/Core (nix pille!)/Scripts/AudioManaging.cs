f266d7f55240c70f2eb173576890160a5e382335
Hargaflarb <emmabrocher@gmail.com>
2025-09-15 14:01:19 +0200

    working on audio manager

--- Assets/Core (nix pille!)/Scripts/AudioManaging.cs
+++ Assets/Core (nix pille!)/Scripts/AudioManaging.cs
@@ -1,16 +1,63 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManaging : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    #region Fields
+    [Header("(Will not change the volume if changed during the game)")]
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float globalVolume;
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float SFXVolume;
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float musicVolume;
+    [SerializeField]
+    private List<AudioSource> soundEffects=new List<AudioSource>();
+    #endregion
+    #region Properties
+    public float Volume
+    { 
+        get 
+        { return globalVolume; }
+        set 
+        { 
+            //updates the volume only when it is changed
+            globalVolume = value;
+            OnVolumeChange();
+        }
+    }
+    #endregion
+    #region Methods
+    void Awake()
     {
-        
+        //soundEffects.Add()
+        //find all sound effects
+        OnVolumeChange();
+        OnSFXVolumeChange();
+        OnMusicVolumeChange();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    void OnVolumeChange()
+    {
+        AudioListener.volume = globalVolume;
+        Debug.Log("Volume changed to " + globalVolume);
+    }
+    void OnSFXVolumeChange()
+    {
+
+    }
+    void OnMusicVolumeChange()
+    {
+
+    }
+    #endregion
 }
