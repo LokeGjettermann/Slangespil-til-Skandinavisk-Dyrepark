85781702701d43db195d505a4ac3381f08178e4c
Eleavate <echoovervad@gmail.com>
2025-09-17 13:48:40 +0200

    List Done

--- Assets/Content/ScriptableObjects/CardList_SO.cs
+++ Assets/Content/ScriptableObjects/CardList_SO.cs
@@ -4,30 +4,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CardList_SO", menuName = "Scriptable Objects/CardList_SO")]
 public class CardList_SO : ScriptableObject
 {
-    private List<Sprite> pictures = new List<Sprite>();
-    private List<string> text = new List<string>();
-    private int number = 0;
-
     public Sprite sprite1;
     public Sprite sprite2;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        number = 0;   
-    }
+    public List<Sprite> sprites = new List<Sprite>();
+    public List<GameObject> pictureObjects = new List<GameObject>();
 
-    public Sprite ChooseSprite()
-    {
-        pictures.Add(sprite1);
-        pictures.Add(sprite2);
-        Sprite value = pictures[number];
-        number++;
-
-        Debug.Log(sprite1);
-        Debug.Log(sprite2);
-
-        pictures.Clear();
-        return value;
-    }
 }
