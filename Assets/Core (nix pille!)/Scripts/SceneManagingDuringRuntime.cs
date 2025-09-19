3e1ac77b15a4a5c22082702cf32289b7bc17a02f
Hargaflarb <emmabrocher@gmail.com>
2025-09-19 13:31:20 +0200

    made it possible to choose which scenes unload

--- Assets/Core (nix pille!)/Scripts/SceneManagingDuringRuntime.cs
+++ Assets/Core (nix pille!)/Scripts/SceneManagingDuringRuntime.cs
@@ -15,16 +15,22 @@ public class sceneManagingDuringRuntime : MonoBehaviour
     private string mainMenuSceneName = "Main menu";
     [SerializeField]
     private bool activateMainMenu = false;
+    [SerializeField]
+    private bool deactivateMainMenu = false;
     [Header("UI")]
     [SerializeField]
     private string uiSceneName = "UI";
     [SerializeField]
-    private bool activateUI = true;
+    private bool activateUI = false;
+    [SerializeField]
+    private bool deactivateUI = false;
     [Header("Sorting minigame")]
     [SerializeField]
     private string sortingGameSceneName = "Sorting minigame";
     [SerializeField]
     private bool activateSortingGame = false;
+    [SerializeField]
+    private bool deactivateSortingGame = false;
     #endregion
     #region Methods
 
@@ -41,17 +47,29 @@ public class sceneManagingDuringRuntime : MonoBehaviour
             }
             SceneManager.SetActiveScene(SceneManager.GetSceneByName(mainSceneName));
         }
-        if (activateMainMenu)
+        if (activateMainMenu && !SceneManager.GetSceneByName(mainMenuSceneName).isLoaded)
         {
             SceneManager.LoadScene(mainMenuSceneName, LoadSceneMode.Additive);
         }
-        if (activateUI)
+        if (activateUI && !SceneManager.GetSceneByName(uiSceneName).isLoaded)
         {
             SceneManager.LoadScene(uiSceneName, LoadSceneMode.Additive);
         }
-        if (activateSortingGame)
+        if (activateSortingGame && !SceneManager.GetSceneByName(sortingGameSceneName).isLoaded)
         {
             SceneManager.LoadScene(sortingGameSceneName, LoadSceneMode.Additive);
+        }
+        if (deactivateMainMenu && SceneManager.GetSceneByName(mainMenuSceneName) != gameObject.scene && SceneManager.GetSceneByName(mainMenuSceneName).isLoaded)
+        {
+            SceneManager.UnloadSceneAsync(SceneManager.GetSceneByName(mainMenuSceneName));
+        }
+        if (deactivateUI && SceneManager.GetSceneByName(uiSceneName) != gameObject.scene && SceneManager.GetSceneByName(uiSceneName).isLoaded)
+        {
+            SceneManager.UnloadSceneAsync(SceneManager.GetSceneByName(uiSceneName));
+        }
+        if (deactivateSortingGame && SceneManager.GetSceneByName(sortingGameSceneName) != gameObject.scene && SceneManager.GetSceneByName(sortingGameSceneName).isLoaded)
+        {
+            SceneManager.UnloadSceneAsync(SceneManager.GetSceneByName(sortingGameSceneName));
         }
         //unload this scene, as it is not needed anymore
         yield return SceneManager.UnloadSceneAsync(gameObject.scene);
